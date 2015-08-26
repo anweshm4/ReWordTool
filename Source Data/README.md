32a98d5d49b9d567a4247e6bcc29d37d65e84c20
Anwesh Mishra <anweshm4@gmail.com>
2015-08-26 20:37:57 +0530

    Cleaned Repo

--- Source Data/README.md
+++
@@ -1,2 +0,0 @@
-# ReWordTool
-Tool built using Python's NLTK to preprocess a sentence and deliver results using the Lesk algorithm.
--- Source Data/leskv4.py
+++
@@ -1,6 +0,0 @@
-from pywsd.lesk import simple_lesk
-sent = ' The path leads us through.'
-ambiguous = 'through'
-answer = simple_lesk(sent, ambiguous)
-print answer
-print answer.definition()
