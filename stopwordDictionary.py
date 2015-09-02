460429d4466b87a194f4c0893168deb68c6bfb12
Amlanjyoti Saikia <amlanjyoti.s@gmail.com>
2015-09-02 12:42:06 +0530

    Removed a stupid bug that messed up CamelCase stop words.

--- stopwordDictionary.py
+++ stopwordDictionary.py
@@ -10,11 +10,13 @@ class StopWordDictionary:
     reader = csv_file = None
 
     def __init__(self):  # Constructor
+        print "Initializing Stop word dictionary..."
         self.csv_file = open('StopwordDefinitions.csv')  # Open the file
         self.reader = csv.reader(self.csv_file, delimiter=':')  # Create _csv.reader object
         return
 
     def has_sense(self, word):  # Returns true if dictionary contains a definition
+        word = word.lower()
         for row in self.reader:  # Iterate through each line
             if word == row[0]:  # If word matches key
                 self.csv_file.seek(0)  # Reset file to start
@@ -22,6 +24,7 @@ class StopWordDictionary:
         return False  # Nothing found, return false
 
     def get_sense(self, word):  # Returns the definition of the word
+        word = word.lower()
         for row in self.reader:  # Iterate through each line
             if word == row[0]:  # If word matches key
                 self.csv_file.seek(0)  # Reset file to start
